4ff9c7682a0a19df0b40312ff8dc8fb1f668bd9e
Caleb Parikh <parikhcaleb@gmail.com>
2018-10-11 20:25:24 +0100

    Update christmasTree.js

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -12,4 +12,21 @@ export const christmasTree = (level) => {
   //   I
 
   // If no level is provided the function should throw an error
+  
+  
+ 	 if(typeof(level) != 'number');
+    {
+      throw new typeError("no level provided")
+    }
+
+  var tree = "";
+    for(i = 0; i < level; i++)
+        {
+        tree += " ".repeat(level-i);
+        tree += "x".repeat(2*i+1) ;
+        tree += "\n";
+        }
+			tree += " ".repeat(level);
+			tree += "I";
+    return tree;
 };
