0ab581894a8c196bb82a7fb84c7b7c11df56f4ef
Caleb Parikh <parikhcaleb@gmail.com>
2018-10-11 22:12:35 +0100

    Update christmas tree Implemented function drawing christmas tree with height of passed parameter

--- src/christmasTree.js
+++ src/christmasTree.js
@@ -12,4 +12,28 @@ export const christmasTree = (level) => {
   //   I
 
   // If no level is provided the function should throw an error
+console.log(typeof level)
+  if(typeof level != 'number')
+ {
+   throw new TypeError("no level provided")
+ }
+
+var tree = "";
+var i = 0;
+for(i = 0; i < level; i++)
+     {
+     tree += " ".repeat(level-i-1);
+     tree += "X".repeat(2*i+1) ;
+     tree += " ".repeat(level-i-1);
+     tree += "\n";
+     }
+   tree += " ".repeat(level-1);
+   tree += "I";
+   tree += " ".repeat(level-1);
+   tree += "\n"
+
+ return tree;
 };
+
+
+console.log(christmasTree(3))
